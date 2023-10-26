e973b247abbe93b2c1b71fd30e44fb6b87e647d0
Golam Mursalin <golammursalin309@gmail.com>
2023-10-26 10:20:41 +0600

    server side sorting and filtering add for booking table data

--- src/pages/Bookings/Bookings.jsx
+++ src/pages/Bookings/Bookings.jsx
@@ -1,4 +1,5 @@
 import BookingsTable from "../../features/bookings/BookingTable/BookingsTable";
+import BookingTableOperations from "../../features/bookings/BookingTableOperations/BookingTableOperations";
 import Heading from "../../ui/Heading/Heading";
 import Row from "../../ui/Row/Row";
 
@@ -7,7 +8,7 @@ function Bookings() {
     <>
       <Row type="horizontal">
         <Heading as="h1">All bookings</Heading>
-        <p>filter / sort</p>
+        <BookingTableOperations />
       </Row>
       <Row>
         <BookingsTable />
--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -1,12 +1,23 @@
 import supabase from "./supabase";
 
 // Get All Bookings
-export async function getBookings() {
-  let { data, error } = await supabase
+export async function getBookings({ filter, sortBy }) {
+  let query = supabase
     .from("bookings")
     .select(
       "id, created_at, startDate, endDate, numNights, numGuests, status, totalPrice, cabins(name), guests(fullName, email)"
     );
+
+  // Filter
+  if (filter) query = query[filter.method || "eq"](filter.field, filter.value);
+
+  // Sort
+  if (sortBy)
+    query = query.order(sortBy.field, {
+      ascending: sortBy.direction === "asc",
+    });
+
+  const { data, error } = await query;
 
   if (error) {
     console.error(error);
